05c53b9f6988ee887141877ac1a68a20681399ea
최연우 <iconic0512@ajou.ac.kr>
2026-06-22 17:15:19 +0900

    Switch ABS engage to ax-based gate, B1 stopping distance near baseline, final 56.30/70

--- icc-project/scripts/control/ctrl_longitudinal.m
+++ icc-project/scripts/control/ctrl_longitudinal.m
@@ -1,5 +1,5 @@
 function [forceCmd, ctrlState] = ctrl_longitudinal(vxRef, vx, ax, ctrlState, CTRL, LIM, dt)
-%CTRL_LONGITUDINAL 속도추종 PI + 게이트 있는 κ=0.12 직접추종 ABS
+%CTRL_LONGITUDINAL 속도추종 PI + ax기반 게이트의 κ=0.12 직접추종 ABS
 
     if ~isfield(ctrlState,'intError');   ctrlState.intError   = 0; end
     if ~isfield(ctrlState,'prevForce');  ctrlState.prevForce  = 0; end
@@ -12,32 +12,26 @@ function [forceCmd, ctrlState] = ctrl_longitudinal(vxRef, vx, ax, ctrlState, CTR
     ctrlState.intError = max(min(ctrlState.intError, CTRL.LON.intMax), -CTRL.LON.intMax);
     Fx_track = CTRL.LON.Kp*err + CTRL.LON.Ki*ctrlState.intError;
 
-    kappaEngage  = 0.15;
-    kappaRelease = 0.05;
-    kappaTarget  = 0.12;
+    axBrakeThreshold = -1.8;   % [m/s^2] 의도적 강한 브레이크 판단 기준 (약 0.2g)
+    kappaTarget = 0.12;
     Fx_abs = 0;
 
-    if ax < 0
-        kappaMeas = mean(abs(ctrlState.wheelSlip));
-
-        if ~ctrlState.absActive && kappaMeas > kappaEngage
-            ctrlState.absActive = true;
-            ctrlState.slipIntErr = 0;
-        elseif ctrlState.absActive && kappaMeas < kappaRelease
-            ctrlState.absActive = false;
-        end
-
-        if ctrlState.absActive
-            kappaErr = kappaMeas - kappaTarget;
-            ctrlState.slipIntErr = ctrlState.slipIntErr + kappaErr*dt;
-            ctrlState.slipIntErr = max(min(ctrlState.slipIntErr, 1), -1);
-            Fx_abs = (CTRL.LON.absKp*kappaErr + CTRL.LON.absKi*ctrlState.slipIntErr) * LIM.MAX_BRAKE_TRQ;
-        end
+    if ax < axBrakeThreshold
+        ctrlState.absActive = true;
     else
         ctrlState.absActive = false;
+        ctrlState.slipIntErr = 0;   % 꺼질 때 적분 초기화
     end
 
-    if ax < 0
+    if ctrlState.absActive
+        kappaMeas = mean(abs(ctrlState.wheelSlip));
+        kappaErr = kappaMeas - kappaTarget;
+        ctrlState.slipIntErr = ctrlState.slipIntErr + kappaErr*dt;
+        ctrlState.slipIntErr = max(min(ctrlState.slipIntErr, 1), -1);
+        Fx_abs = (CTRL.LON.absKp*kappaErr + CTRL.LON.absKi*ctrlState.slipIntErr) * LIM.MAX_BRAKE_TRQ;
+    end
+
+    if ctrlState.absActive
         Fx_des = Fx_abs;
     else
         Fx_des = Fx_track;
